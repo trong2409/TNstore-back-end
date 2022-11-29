170db3a8714045efb1b63e460b6e51bbc45b35d7
trong2409 <nvtrong249@gmail.com>
2022-11-29 20:01:51 +0700

    check

--- routes/products/validate.js
+++ routes/products/validate.js
@@ -59,7 +59,7 @@ export const storeImage = async (req, res, next) => {
     return res.status.json({ message: "Only images are allowed" });
   }
   req.image = req.file.fieldname + "-" + Date.now() + "." + extend;
-  fs.writeFile(rootDir + `/images/${req.image}`, req.file.buffer, (err) => {
+  fs.writeFileSync(rootDir + `/images/${req.image}`, req.file.buffer, (err) => {
     if (err) {
       return res.status(500).json({ message: "Some things wrong" });
     } else {
